d7ef8be6f9c1a10de702d43f45b642330b3b3ed9
ProgramFOX <programfox@hotmail.be>
2016-01-02 12:41:35 +0100

    Add config.json and MongoSettings class

---
+++ src/AtomicChessPuzzles/MongoSettings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AtomicChessPuzzles
+{
+    public class MongoSettings
+    {
+        public MongoSettings()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
+            MongoConnectionString = config.Get<string>("mongo:mongoconnectionstring", null);
+            Database = config.Get<string>("mongo:database", null);
+        }
+        public string MongoConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
