fc53b2310ee0be484fd0b1f9e5060722a7144dd7
ProgramFOX <programfox@hotmail.be>
2016-05-04 18:26:36 +0200

    Add dropdown menu for logged in users

---
+++ src/AtomicChessPuzzles/wwwroot/scripts/main.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener("load", function () {
+    document.getElementById("username").addEventListener("click", function (e) {
+        e.preventDefault();
+        with (document.getElementById("user-nav-content")) {
+            if (getAttribute("class").indexOf("invisible") === -1) {
+                setAttribute("class", "invisible");
+            }
+            else {
+                setAttribute("class", "visible");
+            }
+        }
+    });
+});
