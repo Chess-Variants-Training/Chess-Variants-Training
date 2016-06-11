c83a2943ba4ccc63ffe7d9820ffcf470cd41881b
ProgramFOX <programfox@hotmail.be>
2016-06-11 18:08:37 +0200

    Add CommentModerator role

--- src/AtomicChessPuzzles/Models/UserRole.cs
+++ src/AtomicChessPuzzles/Models/UserRole.cs
@@ -8,7 +8,8 @@ namespace AtomicChessPuzzles.Models
         None = 0,
         PuzzleReviewer = 1,
         PuzzleEditor = 2,
-        UserModerator = 4,
-        Admin = 8
+        CommentModerator = 4,
+        UserModerator = 8,
+        Admin = 16
     }
 }
