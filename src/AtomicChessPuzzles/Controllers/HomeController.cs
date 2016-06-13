eecf4b49ddca3bdc7c42e88d59aac7b76270defb
ProgramFOX <programfox@hotmail.be>
2016-06-13 20:15:29 +0200

    Cleaned up a few usings and comments

--- src/AtomicChessPuzzles/Controllers/HomeController.cs
+++ src/AtomicChessPuzzles/Controllers/HomeController.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+﻿using Microsoft.AspNet.Mvc;
 
 namespace AtomicChessPuzzles
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         [Route("/")]
         public IActionResult Index()
         {
