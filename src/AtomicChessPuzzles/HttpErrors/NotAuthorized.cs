c0d03b15ee9808cb5833e15249901d107750bbc4
ProgramFOX <programfox@hotmail.be>
2016-07-15 09:42:46 +0200

    Not Authorized should be 401

--- src/AtomicChessPuzzles/HttpErrors/NotAuthorized.cs
+++ src/AtomicChessPuzzles/HttpErrors/NotAuthorized.cs
@@ -2,7 +2,7 @@ namespace AtomicChessPuzzles.HttpErrors
 {
     public class NotAuthorized : HttpError
     {
-        public override int StatusCode { get; } = 403;
+        public override int StatusCode { get; } = 401;
         public override string StatusText { get; } = "Not Authorized";
         public NotAuthorized(string desc) : base(desc) { }
     }
