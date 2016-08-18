b250f1b55fc0334602ec0930ebbbfbb45da79070
ProgramFOX <programfox@hotmail.be>
2016-08-18 12:47:51 +0200

    Use BsonElementAttributes in Counter

--- src/AtomicChessPuzzles/Models/Counter.cs
+++ src/AtomicChessPuzzles/Models/Counter.cs
@@ -1,8 +1,13 @@
-﻿namespace AtomicChessPuzzles.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace AtomicChessPuzzles.Models
 {
     public class Counter
     {
+        [BsonElement("_id")]
         public string ID { get; set; }
+
+        [BsonElement("next")]
         public int Next { get; set; }
 
         public Counter(string id, int next)
