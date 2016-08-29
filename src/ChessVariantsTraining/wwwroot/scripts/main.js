94e0d2e4b9e120ee7ff448347a07c43e6a04590a
ProgramFOX <programfox@hotmail.be>
2016-08-29 10:49:36 +0200

    Make #user-nav-content a dropdown menu as well

--- src/ChessVariantsTraining/wwwroot/scripts/main.js
+++ src/ChessVariantsTraining/wwwroot/scripts/main.js
@@ -1,28 +1,4 @@
-﻿function getUsernameFromTopbar() {
-    var usernameElement = document.getElementById("username");
-    if (!usernameElement) {
-        return "Anonymous";
-    }
-    return usernameElement.textContent;
-}
-
-window.addEventListener("load", function () {
-    if (document.getElementById("username")) {
-        document.getElementById("username").addEventListener("click", function (e) {
-            e.preventDefault();
-            with (document.getElementById("user-nav-content")) {
-                if (getAttribute("class").indexOf("invisible") === -1) {
-                    setAttribute("class", "invisible");
-                }
-                else {
-                    setAttribute("class", "visible");
-                }
-            }
-        });
-    }
-});
-
-function xhr(url, method, data, success, error) {
+﻿function xhr(url, method, data, success, error) {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         if (req.readyState === 4 && req.status === 200) {
