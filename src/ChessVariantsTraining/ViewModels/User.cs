4f67529c57d7f85e7e64ccdbda39fe3059bf9422
ProgramFOX <programfox@hotmail.be>
2016-09-17 16:50:09 +0200

    Improve profile page a bit

--- src/ChessVariantsTraining/ViewModels/User.cs
+++ src/ChessVariantsTraining/ViewModels/User.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ChessVariantsTraining.ViewModels
 {
@@ -10,21 +9,11 @@ namespace ChessVariantsTraining.ViewModels
         public int PuzzlesCorrect { get; private set; }
         public int PuzzlesWrong { get; private set; }
         public List<string> Roles { get; private set; }
-        public int Rating { get; private set; }
         public int PuzzlesMade
         {
             get
             {
                 return PuzzlesCorrect + PuzzlesWrong;
-            }
-        }
-
-        public float PercentageCorrect
-        {
-            get
-            {
-                if (PuzzlesMade == 0) return 0;
-                return (float)Math.Round(PuzzlesCorrect / (float)PuzzlesMade * 100, 1, MidpointRounding.AwayFromZero);
             }
         }
 
@@ -40,7 +29,6 @@ namespace ChessVariantsTraining.ViewModels
             PuzzlesCorrect = user.PuzzlesCorrect;
             PuzzlesWrong = user.PuzzlesWrong;
             Roles = user.Roles;
-            Rating = (int)user.Ratings["Atomic"].Value;
         }
     }
 }
