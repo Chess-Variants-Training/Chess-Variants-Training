b22a90f9586a1b6ee34c8d4cf48540b24f8ddcb5
ProgramFOX <programfox@hotmail.be>
2016-10-05 10:20:42 +0200

    Add ToS and Privacy Policy

---
+++ src/ChessVariantsTraining/Controllers/MiscController.cs
@@ -0,0 +1,24 @@
+﻿using ChessVariantsTraining.DbRepositories;
+using ChessVariantsTraining.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessVariantsTraining.Controllers
+{
+    public class MiscController : CVTController
+    {
+
+        public MiscController(IUserRepository _userRepository, IPersistentLoginHandler _loginHandler) : base(_userRepository, _loginHandler) { }
+
+        [Route("/Terms-of-Service")]
+        public IActionResult TermsOfService()
+        {
+            return View();
+        }
+
+        [Route("/Privacy-Policy")]
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
