fc85f37eb606d44f101bf5fef3b3e89460f1b410
ProgramFOX <programfox@hotmail.be>
2017-01-01 14:41:54 +0100

    Add IGameRepository interface

---
+++ src/ChessVariantsTraining/DbRepositories/Variant960/IGameRepository.cs
@@ -0,0 +1,11 @@
+﻿using ChessVariantsTraining.Models.Variant960;
+
+namespace ChessVariantsTraining.DbRepositories.Variant960
+{
+    public interface IGameRepository
+    {
+        void Add(Game game);
+        void Get(string id);
+        void Update(Game game);
+    }
+}
