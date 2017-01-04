7b54f10a2e83e3752f4a27ca709e1ca73075108a
ProgramFOX <programfox@hotmail.be>
2017-01-04 11:38:42 +0100

    Add Game view model

---
+++ src/ChessVariantsTraining/ViewModels/Game.cs
@@ -0,0 +1,51 @@
+﻿namespace ChessVariantsTraining.ViewModels
+{
+    public class Game
+    {
+        public string White
+        {
+            get;
+            private set;
+        }
+
+        public string Black
+        {
+            get;
+            private set;
+        }
+
+        public string WhiteUrl
+        {
+            get;
+            private set;
+        }
+
+        public string BlackUrl
+        {
+            get;
+            private set;
+        }
+
+        public string Variant
+        {
+            get;
+            private set;
+        }
+
+        public string TimeControl
+        {
+            get;
+            private set;
+        }
+
+        public Game(string white, string black, string whiteUrl, string blackUrl, string variant, string timeControl)
+        {
+            White = white;
+            Black = black;
+            WhiteUrl = whiteUrl;
+            BlackUrl = blackUrl;
+            Variant = variant;
+            TimeControl = timeControl;
+        }
+    }
+}
