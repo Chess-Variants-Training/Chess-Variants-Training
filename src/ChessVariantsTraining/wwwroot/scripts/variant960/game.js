c8c39ac74d954d4c82ecd6f6b997b8f23e11f71d
ProgramFOX <programfox@hotmail.be>
2017-02-14 22:07:27 +0100

    /Variant960/Game/...: display board based on FEN

--- src/ChessVariantsTraining/wwwroot/scripts/variant960/game.js
+++ src/ChessVariantsTraining/wwwroot/scripts/variant960/game.js
@@ -2,6 +2,7 @@
     var ground;
     window.addEventListener("load", function () {
         ground = Chessground(document.getElementById("chessground"), {
+            fen: window.fen,
             coordinates: true,
             movable: {
                 free: false,
