56da4c1fe551f748146d577580fe650173d98d78
ProgramFOX <programfox@hotmail.be>
2017-02-22 19:21:26 +0100

    Add ChatMessage model

---
+++ src/ChessVariantsTraining/Models/Variant960/ChatMessage.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChessVariantsTraining.Models.Variant960
+{
+    public class ChatMessage
+    {
+        [BsonElement("userId")]
+        public int? UserID { get; set; }
+
+        [BsonElement("displayName")]
+        public string DisplayName { get; set; }
+
+        [BsonElement("content")]
+        public string Content { get; set; }
+
+        public ChatMessage() { }
+
+        public ChatMessage(int? userId, string displayName, string content)
+        {
+            UserID = userId;
+            DisplayName = displayName;
+            Content = content;
+        }
+    }
+}
