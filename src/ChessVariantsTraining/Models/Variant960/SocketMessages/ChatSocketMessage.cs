2486b671a8d9f8f3bb8f2f810e68a1fa5520eae3
ProgramFOX <programfox@hotmail.be>
2017-02-26 15:42:08 +0100

    Add ChatSocketMessage class

---
+++ src/ChessVariantsTraining/Models/Variant960/SocketMessages/ChatSocketMessage.cs
@@ -0,0 +1,47 @@
+﻿namespace ChessVariantsTraining.Models.Variant960.SocketMessages
+{
+    public class ChatSocketMessage : GameSocketMessage
+    {
+        public string Content { get; private set; }
+        public string Channel { get; private set; }
+
+        public ChatSocketMessage(GameSocketMessage gms) : base()
+        {
+            Okay = gms.Okay;
+            if (!Okay) return;
+
+            Type = gms.Type;
+            if (DeserializedDictionary.ContainsKey("d"))
+            {
+                Content = DeserializedDictionary["d"] as string;
+            }
+            else
+            {
+                Okay = false;
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(Content))
+            {
+                Okay = false;
+                return;
+            }
+
+            if (DeserializedDictionary.ContainsKey("channel"))
+            {
+                Channel = DeserializedDictionary["channel"] as string;
+            }
+            else
+            {
+                Okay = false;
+                return;
+            }
+
+            if (Channel != "player" || Channel != "spectator")
+            {
+                Okay = false;
+                return;
+            }
+        }
+    }
+}
