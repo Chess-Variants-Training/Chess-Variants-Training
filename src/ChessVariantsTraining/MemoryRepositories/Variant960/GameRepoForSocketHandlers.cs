56fcfa559527a31d79cfa0c9052bdb9c227cb86f
ProgramFOX <programfox@hotmail.be>
2017-02-27 19:40:59 +0100

    Stop server-side clock stopwatch on game termination

--- src/ChessVariantsTraining/MemoryRepositories/Variant960/GameRepoForSocketHandlers.cs
+++ src/ChessVariantsTraining/MemoryRepositories/Variant960/GameRepoForSocketHandlers.cs
@@ -60,6 +60,8 @@ namespace ChessVariantsTraining.MemoryRepositories.Variant960
 
         public void RegisterGameOutcome(Game subject, string outcome)
         {
+            subject.ClockWhite.Pause();
+            subject.ClockBlack.Pause();
             subject.Outcome = outcome;
             subject.EndedUtc = DateTime.UtcNow;
             gameRepository.Update(subject);
