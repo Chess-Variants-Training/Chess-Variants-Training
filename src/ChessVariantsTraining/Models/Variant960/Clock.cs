0c8d38db8c867fefa88270385b651de69b1381e4
ProgramFOX <programfox@hotmail.be>
2017-02-27 02:36:04 +0100

    WIP Clock class

---
+++ src/ChessVariantsTraining/Models/Variant960/Clock.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace ChessVariantsTraining.Models.Variant960
+{
+    public class Clock
+    {
+        double secondsLimit;
+        Stopwatch stopwatch;
+        TimeControl timeControl;
+
+        public Clock(TimeControl tc)
+        {
+            timeControl = tc;
+            secondsLimit = tc.InitialSeconds;
+        }
+
+        public void Start()
+        {
+            stopwatch.Start();
+        }
+
+        public void Pause()
+        {
+            stopwatch.Stop();
+        }
+
+        public void AddIncrement()
+        {
+            secondsLimit += timeControl.Increment;
+        }
+
+        public double GetSecondsLeft()
+        {
+            return secondsLimit - stopwatch.Elapsed.TotalSeconds;
+        }
+    }
+}
